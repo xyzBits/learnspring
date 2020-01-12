2cb4418fab316a3d5af9affac56fcf155e45bc5e
lidf077 <lidf077@gmail.com>
2020-01-12 17:22:05 +0800

    配置文件变更

--- src/main/java/com/dongfang/spring/ssm/controller/EmployeeController.java
+++ src/main/java/com/dongfang/spring/ssm/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.dongfang.spring.ssm.controller;
 import com.dongfang.spring.ssm.bean.Employee;
 import com.dongfang.spring.ssm.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +18,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping(path = "/employees", method = RequestMethod.GET)
+    @RequestMapping(path = "/employees", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
